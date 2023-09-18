9fd8657ca139f7f913b8a931a9cb642810a8033f
stilyanpetrov <f.and.g.2019@gmail.com>
2023-09-18 22:40:48 +0300

    First Commit

---
+++ src/main/java/bg/BulgariaTripPlanner/BulgariaTripPlannerApplication.java
@@ -0,0 +1,13 @@
+package bg.BulgariaTripPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulgariaTripPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulgariaTripPlannerApplication.class, args);
+	}
+
+}
