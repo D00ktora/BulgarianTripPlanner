e995fae94ae1343915ba6a12270f9f47fe176ac6
stilyanpetrov <f.and.g.2019@gmail.com>
2023-09-26 12:30:41 +0300

    Fixed problem with switching between Login form and Register form. Now if register form is open and user click on login button, the form will change automatically and vici-versa.

---
+++ src/main/resources/templates/script.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector('.wrapper');
+const logInLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnLogin = document.querySelector('.btnLogin-popup');
+const btnRegister = document.querySelector('.btnRegister-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+})
+
+logInLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+})
+btnLogin.addEventListener('click', ()=> {
+    if (wrapper.classList.contains('active-popup-register')) {
+    wrapper.classList.add('active-popup');
+    wrapper.classList.remove('active-popup-register');
+    } else {
+        wrapper.classList.add('active-popup');
+    }
+})
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+})
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup-register');
+})
+btnRegister.addEventListener('click', ()=> {
+    if (wrapper.classList.contains('active-popup')){
+    wrapper.classList.add('active-popup-register');
+    wrapper.classList.remove('active-popup');
+    } else {
+        wrapper.classList.add('active-popup-register');
+    }
+})
